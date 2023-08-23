a34bfb23abbac55d33bb02c1ae06c13605a04d40
Alleksss <Solidnuii@gmail.com>
2023-08-24 00:18:00 +0300

    finish_task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredients = document.querySelector('#ingredients');
+const liElements = ingredients.map(ingredient => {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add('item');
+  return li;
+});
+
+listIngredients.append(...liElements);
+
+
+// HTML містить порожній список ul#ingredients.
+
+// <ul id="ingredients"></ul>
+
+
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
---
+++ js/task-07.js
@@ -0,0 +1,14 @@
+const fontSizeControl = document.querySelector('#font-size-control');
+const textSpan = document.querySelector('#text');
+
+fontSizeControl.addEventListener('input', () => {
+    const fontSize = fontSizeControl.value + 'px';
+    textSpan.style.fontSize = fontSize;
+});
+
+
+// Напиши скрипт, який реагує на зміну значення input#font-size-control (подія input) і змінює інлайн-стиль span#text, оновлюючи властивість font-size. В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
+
+// <input id="font-size-control" type="range" min="16" max="96" />
+// <br />
+// <span id="text">Abracadabra!</span>
